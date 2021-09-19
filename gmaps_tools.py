3a74388787416acbfa3a3319cc67e7fd2e9794ee
Christian Steinhofer <cmsteinhofer@gmail.com>
2021-09-19 01:00:53 -0500

    oops api key

--- gmaps_tools.py
+++ gmaps_tools.py
@@ -1,9 +1,9 @@
 import googlemaps
-API_key = 'AIzaSyAalBg0cbYTnXXWzkr3jkanvpUAhA2WdSA'
+
 
 gmaps = googlemaps.Client(key=API_key)
 
-origin = gmaps.revese_geocode(29.755537,-95.372003)
-destination = gmaps.reverse_geocode(29.716361,-95.409329)
+origin = (29.755537,-95.372003)
+destination = (29.716361,-95.409329)
 
 matrix = gmaps.distance_matrix(origins=origin, destinations=destination, mode='driving', traffic_model='best_guess')
