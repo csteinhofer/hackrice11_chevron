5ec9d8b07eac46b2cae75dc1af84eab72c675aa5
Christian Steinhofer <cmsteinhofer@gmail.com>
2021-09-18 23:34:37 -0500

    starting skeleton to simulate a work day

---
+++ demo_driver.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+start_dt = pd.to_datetime('2018-09-12 05:00:00')
+
+
+def simulate_workday(start):
+    """
+    simulates a day of work with realtime schedule updates
+    :param start: start timestamp @ 5am
+    :return: none
+    """
+    dti = pd.date_range(start, periods=15, freq='H')
+    for datetime in dti:
+        pass
+
+
+simulate_workday(start_dt)
