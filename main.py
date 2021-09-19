6b770da15f7eedda628142066100e326d76a906b
Christian Steinhofer <cmsteinhofer@gmail.com>
2021-09-18 22:35:37 -0500

    created template folder and moved @briatw's html to it

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # main.py
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -8,6 +8,9 @@ app = Flask(__name__)
 def home():
     return 'Hello World'
 
+@app.route('/submit')
+def submit():
+    return render_template('submit.html')
 
 if __name__ == '__main__':
     app.run()
